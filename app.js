1a312d377eba928191874f49f40789807c509612
Ioannis Kostomanolakis <ioannis.kostomanolakis@hauptmedia.de>
2012-05-10 14:43:33 +0200

    Remoting API inclusion changed

--- app.js
+++ app.js
@@ -3,9 +3,19 @@ Ext.require([
     'Ext.data.*'
 ]);
 
-Ext.onReady(function() {
-    Ext.direct.Manager.addProvider( Ext.app.REMOTING_API )
+var apis = [
+    '/api',
+    'http://localhost:8080/api'
+]
 
+var after = function(times, func) {
+    if (times <= 0) return func();
+    return function() {
+        if (--times < 1) { return func.apply(this, arguments); }
+    };
+};
+
+Ext.onReady(function() {
     Ext.override( Ext.data.Store, {
         loadDataViaReader : function(data, append) {
             var me      = this,
@@ -16,81 +26,102 @@ Ext.onReady(function() {
             me.fireEvent('load', me, result.records, true)
         }
     })
+
+    var lock = after( apis.length, startEditor )
+
+    Ext.each( apis, function( api ) {
+            Ext.Ajax.request({
+                url: api,
+                disableCaching : false,
+                success: function( response ){
+                    var text = response.responseText
+
+                    Ext.direct.Manager.addProvider( JSON.parse( text ) )
+
+                    lock()
+                }
+            })
+        }
+    )
+
 })
 
-Ext.application({
-    name: 'Spelled',
+var startEditor = function() {
 
-    appFolder: 'app',
+    Ext.application({
+        name: 'Spelled',
 
-    controllers: [
-        'Zones',
-        'Entities',
-        'Menu',
-        'Projects',
-        'Components',
-        'Assets',
-        'Blueprints'
-    ],
+        appFolder: 'app',
 
-    stores: [
-        'blueprint.Components',
-        'blueprint.Entities'
-    ],
+        controllers: [
+            'Zones',
+            'Entities',
+            'Menu',
+            'Projects',
+            'Components',
+            'Assets',
+            'Blueprints'
+        ],
 
-    project: undefined,
-    zone: undefined,
+        stores: [
+            'blueprint.Components',
+            'blueprint.Entities'
+        ],
 
-    createTab: function( tabPanel, view ) {
+        project: undefined,
+        zone: undefined,
 
-        var newPanel  = tabPanel.add(
-            view
-        )
+        createTab: function( tabPanel, view ) {
 
-        tabPanel.setActiveTab( newPanel )
+            var newPanel  = tabPanel.add(
+                view
+            )
 
-        return newPanel
-    },
+            tabPanel.setActiveTab( newPanel )
 
-    findActiveTabByTitle: function( tabPanel, title ) {
-        var panels = tabPanel.items.items
+            return newPanel
+        },
 
-        //looking for hidden tabs. returning if we found one
-        for( var key in panels  ) {
-            if( panels[ key ].title === title ) {
-                return panels[ key ]
+        findActiveTabByTitle: function( tabPanel, title ) {
+            var panels = tabPanel.items.items
+
+            //looking for hidden tabs. returning if we found one
+            for( var key in panels  ) {
+                if( panels[ key ].title === title ) {
+                    return panels[ key ]
+                }
             }
+
+            return undefined
+        },
+
+        getActiveProject: function() {
+            return this.project
+        },
+
+        setActiveProject: function( project ) {
+            this.project = project
+        },
+
+        getActiveZone: function() {
+            return this.zone
+        },
+
+        setActiveZone: function( zone ) {
+            this.zone = zone
+        },
+
+        launch: function() {
+
+            Spelled.EntityBlueprintActions.getAll( function( provider, response ) {
+                Ext.getStore('blueprint.Entities').loadDataViaReader( response.result )
+            })
+
+            Spelled.ComponentBlueprintActions.getAll( function( provider, response ) {
+                Ext.getStore('blueprint.Components').loadDataViaReader( response.result )
+            })
+
+            Ext.create('Spelled.view.ui.SpelledViewport')
         }
-
-        return undefined
-    },
-
-    getActiveProject: function() {
-        return this.project
-    },
-
-    setActiveProject: function( project ) {
-        this.project = project
-    },
-
-    getActiveZone: function() {
-        return this.zone
-    },
-
-    setActiveZone: function( zone ) {
-        this.zone = zone
-    },
-
-    launch: function() {
-
-        Spelled.EntityBlueprintActions.getAll( function( provider, response ) {
-            Ext.getStore('blueprint.Entities').loadDataViaReader( response.result )
-        })
-
-        Spelled.ComponentBlueprintActions.getAll( function( provider, response ) {
-            Ext.getStore('blueprint.Components').loadDataViaReader( response.result )
-        })
-
-        Ext.create('Spelled.view.ui.SpelledViewport')
-    }
-});
+    })
+}
