03ddf952cb129864c80708beb293c3a89cd44ecc
Ioannis Kostomanolakis <ioannis.kostomanolakis@spielmeister.com>
2012-07-16 15:13:08 +0200

    SPELLJS-190 - Only show assets with static appearance in the 2D Appearance Component # Resolved

--- public/app/view/component/Properties.js
+++ public/app/view/component/Properties.js
@@ -21,13 +21,38 @@ Ext.define('Spelled.view.component.Properties', {
 			handler:  Ext.bind( me.handleDocClick, me)
 		}]
 
+		var storeId = 'asset.Assets'
+
+		if( !!this.source.assetId ) {
+			switch( this.getAssetIdType( this.source.assetId ) ) {
+				case 'appearance':
+					storeId = 'asset.Textures'
+					break
+				case 'animation':
+					storeId = 'asset.Animations'
+					break
+				case 'spriteSheet':
+					storeId = 'asset.SpriteSheet'
+					break
+			}
+		}
+
 		me.customEditors = {
-			assetId:  new Ext.grid.CellEditor({ field: 'assetidproperty' })
+			assetId:  new Ext.grid.CellEditor({
+				field: {
+					xtype: 'assetidproperty',
+					store: storeId
+				}
+			})
 		}
 
 		if( this.isAdditional === true ) this.closable = true
 
 		this.callParent( arguments )
+	},
+
+	getAssetIdType: function( assetId ) {
+		return assetId.split( ':').shift()
 	},
 
 	handleDocClick: function( event, toolEl, panel ) {
