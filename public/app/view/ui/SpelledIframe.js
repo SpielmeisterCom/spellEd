460df79461101af387852216fc8a486cbe4403d8
Julian Haupt <jh@spielmeister.com>
2012-07-25 00:24:59 +0200

    use output directory and not public

--- public/app/view/ui/SpelledIframe.js
+++ public/app/view/ui/SpelledIframe.js
@@ -12,7 +12,7 @@ Ext.define('Spelled.view.ui.SpelledIframe', {
     },
 
     afterRender: function() {
-        this.el.dom.src = '/' + this.projectName + '/public/spellEdShim.html?iframeId='+this.id
+        this.el.dom.src = '/' + this.projectName + '/output/spellEdShim.html?iframeId='+this.id
 		this.focus()
     }
 });
