a0ec0785abdc1aac568937b5b6377d819c0a0a31
Julian Haupt <jh@spielmeister.com>
2012-07-24 23:52:53 +0200

    make port and project path configurable

--- src/server/spellEdServer.js
+++ src/server/spellEdServer.js
@@ -1,44 +1,83 @@
-require( { baseUrl: "src" } )
-
-require(
+define(
+	'server/spellEdServer',
     [
         'connect',
         'http',
         'connect-extdirect/extDirect',
-        'server/createExtDirectApi'
+        'server/createExtDirectApi',
+	    'commander',
+	    'fs',
+	    'path'
     ],
     function(
         connect,
         http,
         extDirect,
-        createExtDirectApi
+        createExtDirectApi,
+        commander,
+        fs,
+        path
     ) {
         "use strict"
 
-        var projectsRoot        = 'projects/',
-			buildServerOptions  = {
-				host: 'localhost',
-				port: '8080',
-				path: '/router/',
-				method: 'POST'
-			}
+	    return function(argv, cwd, spellPath) {
+		    var executableName  = 'server'
 
-        var app = connect()
-            .use( connect.favicon() )
-            .use( connect.logger('dev') )
-            .use(
-                extDirect(
-                    'router/',
-                    'Spelled',
-                    createExtDirectApi( projectsRoot, buildServerOptions )
-                )
-            )
-            .use( connect.static('public'))
-            //TODO: remove this line
-            .use( connect.static( projectsRoot ) )
 
-        http.Server(app).listen(3000);
-        console.log('Server started on port 3000');
+		    var startServerCommand = function( command ) {
+			    var errors = [],
+				    port = command.port || 3000,
+				    bport = command.bport || 8080,
+				    projectsPath = path.resolve( cwd + ( command.projectsRoot ? '/' + command.projectsRoot : '/projects' )) + '/',
+				    buildServerOptions  = {
+					    host: 'localhost',
+					    port: bport,
+					    path: '/router/',
+					    method: 'POST'
+				    }
+
+			    if( !fs.existsSync( projectsPath ) ) {
+				    errors.push( 'Error: No valid projects directory supplied. Unable to start spelled server. ' +
+					    'See \'' + executableName + ' start-server --help\'.' )
+			    }
+
+			    if( errors.length > 0 ) {
+				    printErrors( errors )
+
+			    } else {
+				    var app = connect()
+					    .use( connect.favicon() )
+					    .use( connect.logger('dev') )
+					    .use(
+					    extDirect(
+						    'router/',
+						    'Spelled',
+						    createExtDirectApi( projectsPath, buildServerOptions )
+					    )
+				    )
+					    .use( connect.static('public'))
+					    //TODO: remove this line
+					    .use( connect.static( projectsPath ) )
+
+				    http.Server(app).listen(port);
+				    console.log('Server started on port ' + port + ' and will use project path ' + projectsPath + ' and connect to a build server at port ' + bport);
+			    }
+		    }
+
+
+		    commander
+			    .version( '0.0.1' )
+
+		    commander
+			    .command( 'start-server' )
+			    .option( '-p, --port [port number]', 'the port the server runs on' )
+			    .option( '-b, --bport [port number]', 'the port the build server is listening' )
+			    .option( '-r, --projects-root [directory]', 'The path to the projects directory that contains the project directories. The default is the current working directory.' )
+			    .description( 'start the spelled server' )
+			    .action( startServerCommand )
+
+		    commander.parse( argv )
+	    }
     }
 )
 
