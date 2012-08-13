7dadb939853f096260dbe0f1606c9ec113f6ac24
Martin Dederer <md@spielmeister.com>
2012-08-13 10:52:25 +0200

    WIP - Added list of template- and asset ids to project configuration structure
     * The list of required template- and asset ids is added to the project config statically whenever it is sent to the engine in development mode. This is a temporary fix und must be refactored.

--- public/libs/spell/editor/createRuntimeModule.js
+++ public/libs/spell/editor/createRuntimeModule.js
@@ -12,7 +12,50 @@ define(
 		 * Creates a runtime module out of a project in editor format.
 		 */
 		return function( project ) {
-			return projectConverter.toEngineFormat( project.getProxy().getWriter().getRecordData( project ) )
+			var projectInEngineFormat = projectConverter.toEngineFormat( project.getProxy().getWriter().getRecordData( project ) )
+
+			// WORKAROUND: The list of templates and assets is included statically. The editor should actually keep these updated. Until this is implemented
+			// this workaround is necessary for the implementation of the development mode.
+
+			projectInEngineFormat.templateIds = [
+				'spell.component.2d.graphics.animatedAppearance',
+				'spell.component.2d.graphics.appearance',
+				'spell.component.2d.graphics.camera',
+				'spell.component.2d.physics.inertialObject',
+				'spell.component.2d.transform',
+				'spell.component.actor',
+				'spell.component.entityComposite.children',
+				'spell.component.entityComposite.root',
+				'spell.component.inputDefinition',
+				'spell.component.physics.collisionSphere',
+				'spell.component.visualObject',
+				'spell.entity.2d.graphics.camera',
+				'spell.entity.inputDefinition',
+				'spell.system.keyInput',
+				'spell.system.render',
+				'spellReferenceProject.component.spacecraft',
+				'spellReferenceProject.entity.alienSpacecraft',
+				'spellReferenceProject.entity.asteroid',
+				'spellReferenceProject.entity.spacecraft',
+				'spellReferenceProject.system.aiControl',
+				'spellReferenceProject.system.collisionDetector',
+				'spellReferenceProject.system.endlessPlayingField',
+				'spellReferenceProject.system.spacecraftIntegrator'
+			]
+
+			projectInEngineFormat.assetIds = [
+				'spell.defaultAppearance',
+				'spellReferenceProject.asteroid1',
+				'spellReferenceProject.asteroid2',
+				'spellReferenceProject.pixelShip1',
+				'spellReferenceProject.pixelShip2',
+				'spellReferenceProject.shield',
+				'spellReferenceProject.spelljsLogo',
+				'spellReferenceProject.threeRingsAnimation',
+				'spellReferenceProject.threeRingsSpriteSheet'
+			]
+
+			return projectInEngineFormat
 		}
 	}
 )
