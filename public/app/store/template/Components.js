6cd4eea5b3abd5a849db0e9891976a262465fa80
Ioannis Kostomanolakis <ik@spielmeister.com>
2012-09-05 11:05:12 +0200

    SPELLJS-368 - Blacklist special engine internal components - Resolved

--- public/app/store/template/Components.js
+++ public/app/store/template/Components.js
@@ -1,5 +1,17 @@
 Ext.define('Spelled.store.template.Components', {
-    extend: 'Spelled.abstract.store.Template',
+	extend: 'Spelled.abstract.store.Template',
 
-    model: 'Spelled.model.template.Component'
+	model: 'Spelled.model.template.Component',
+
+	filters: [
+		function( item ) {
+			var internalSpellComponents = [
+				'spell.component.entityComposite.root',
+				'spell.component.entityComposite.children',
+				'spell.component.name'
+			]
+
+			return !Ext.Array.contains( internalSpellComponents, item.getFullName() )
+		}
+	]
 });
