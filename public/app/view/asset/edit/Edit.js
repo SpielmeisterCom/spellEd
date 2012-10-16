6c6af06769052b296f5fbbf73d828f4b59aaaab5
Ioannis Kostomanolakis <ik@spielmeister.com>
2012-10-16 12:03:52 +0200

    SPELLJS-525 - Don't show useless top form for assets in edit mode - Resolved

--- public/app/view/asset/edit/Edit.js
+++ public/app/view/asset/edit/Edit.js
@@ -7,40 +7,12 @@ Ext.define('Spelled.view.asset.edit.Edit', {
 
 	items: [
 		{
-			xtype: "combo",
-			disabled: true,
-			readOnly: true,
-			name: 'subtype',
-			editable: false,
-			emptyText: '-- Select Type --',
-			store: 'asset.Types',
-			queryMode: 'local',
-			fieldLabel: "Type",
-			displayField: 'name',
-			valueField: 'type'
-		},
-		{
-			xtype: "textfield",
-			disabled: true,
-			name: 'name',
-			fieldLabel: 'Name'
-		},
-		{
-			xtype: "textfield",
-			disabled: true,
-			name: 'namespace',
-			fieldLabel: 'Namespace'
-		},
-		{
 			xtype: 'filefield',
 			name: 'asset',
 			fieldLabel: 'File',
 			labelWidth: 50,
 			msgTarget: 'side',
 			buttonText: 'Select a File...'
-		},
-		{
-			xtype: 'menuseparator'
 		},{
 			xtype: 'spritesheetconfig',
 			hidden: true
