71d307aa6db01da1a8024dbbb553509b73a9fc6a
Julian Haupt <jh@spielmeister.com>
2012-12-10 00:49:35 +0100

    Version 0.7.6 release

--- public/app-configuration.js
+++ public/app-configuration.js
@@ -5,8 +5,8 @@
 Ext.ns("Ext.app");
 
 Ext.app.CONFIGURATION = {
-        "version" : "0.7.5",
+        "version" : "0.7.6",
 	"storageVersion": 1,
         "name" : "SpellEd",
-        "documentationServerURL": "http://docs.spelljs.com/0.7.5/"
+        "documentationServerURL": "http://docs.spelljs.com/0.7.6/"
 };
