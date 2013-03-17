9cede54ad9c7e16909e095fbd9fa502ba33af4c3
Julian Haupt <julian.haupt@spielmeister.com>
2013-03-17 19:38:59 +0100

    Added an ExtDirect remoting singleton (so that we can require it in other modules)

---
+++ public/app/Remoting.js
@@ -0,0 +1,32 @@
+Ext.define( 'Spelled.Remoting', {
+	singleton: true,
+
+	requires: [
+		'Ext.direct.*'
+	],
+
+	constructor: function() {
+		Ext.Direct.addProvider(
+		{
+			"url":"/router/",
+			"namespace":"Spelled",
+			"type":"remoting",
+			"actions":{
+				"StorageActions":[
+					{"name":"create","len":1},
+					{"name":"createNamespaceFolder","len":1},
+					{"name":"read","len":1},
+					{"name":"update","len":1},
+					{"name":"destroy","len":1},
+					{"name":"getNamespaces","len":1},
+					{"name":"deleteFolder","len":1}
+				],
+
+				"SpellBuildActions":[
+					{"name":"initDirectory","len":2},
+					{"name":"exportDeployment","len":2}
+				]
+			}
+		})
+	}
+})
