3e776ed24f5f7601926b1c0659a9615f82314c9e
Ioannis Kostomanolakis <ik@spielmeister.com>
2013-04-10 11:51:51 +0200

    SPELLJS-1093 - Repair export and init projects - Removed hardcoded ExtDirectRouterUrl

--- public/app/Configuration.js
+++ public/app/Configuration.js
@@ -2,7 +2,7 @@ Ext.define( 'Spelled.Configuration', {
 	singleton		: true,
 	version			: '0.8.1',
 	storageVersion		: 1,
-	extDirectRouterUrl	: 'http://127.0.0.1:3000/router/',
+	extDirectRouterUrl	: '/router/',
         name			: "SpellEd",
         "documentationServerURL": "http://docs.spelljs.com/0.8.1/"
 
