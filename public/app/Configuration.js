8f7b5ec57635f2a01211f88130205112a25199e7
Julian Haupt <jh@spielmeister.com>
2013-04-30 01:55:55 +0200

    added buildNumber and buildTimeStamp to configuration

--- public/app/Configuration.js
+++ public/app/Configuration.js
@@ -28,7 +28,9 @@ Ext.define( 'Spelled.Configuration', {
 
 	},
 
-	version                : '0.8.1',
+	version                : '9.9.9',
+	buildNumber	       : '99999',
+	buildTimeStamp	       : '2099-01-01T01:00:00.000+01:00',
 	storageVersion         : 1,
 	extDirectRouterUrl     : '/router/',
 	name                   : "SpellEd",
