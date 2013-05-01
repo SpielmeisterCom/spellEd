e9fdd5e472e459fb9388256720df5ea6821e7319
Julian Haupt <julian.haupt@spielmeister.com>
2013-05-01 02:52:11 +0200

    Changed version to 0.8.6

--- public/app/Configuration.js
+++ public/app/Configuration.js
@@ -28,7 +28,7 @@ Ext.define( 'Spelled.Configuration', {
 
 	},
 
-	version                : '0.8.5',
+	version                : '0.8.6',
 	buildNumber	       : '99999',
 	buildTimeStamp	       : '2099-01-01T01:00:00.000+01:00',
 	storageVersion         : 1,
