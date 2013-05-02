46e39047f495f0cedf7fe339c95c23bf5f07e806
Ioannis Kostomanolakis <ik@spielmeister.com>
2013-05-02 15:13:36 +0200

    SPELLJS-1173 - Creating a project with the same name fails - Resolved

--- src/server/extDirectApi/initDirectory.js
+++ src/server/extDirectApi/initDirectory.js
@@ -1,16 +1,34 @@
 define(
 	'server/extDirectApi/initDirectory',
 	[
+		'fs',
 		'path',
 		'child_process'
 	],
 	function(
+		fs,
 		path,
 		childProcess
 	) {
 		'use strict'
 
 		var appendExtension = process.platform == 'win32' ? '.exe' : ''
+
+		var deleteFolderRecursive = function(path) {
+			var files = [];
+			if( fs.existsSync(path) ) {
+				files = fs.readdirSync(path);
+				files.forEach(function(file,index){
+					var curPath = path + "/" + file;
+					if(fs.statSync(curPath).isDirectory()) { // recurse
+						deleteFolderRecursive(curPath);
+					} else { // delete file
+						fs.unlinkSync(curPath);
+					}
+				});
+				fs.rmdirSync(path);
+			}
+		}
 
 		/*
 		 * RPC call handler
@@ -25,6 +43,8 @@ define(
 		 */
 		return function( spellCorePath, workspacePath, spellCliPath, isDevEnvironment, projectName, onComplete ) {
 			var projectPath = path.join( workspacePath , projectName )
+
+			deleteFolderRecursive( projectPath)
 
 			childProcess.execFile( spellCliPath + appendExtension, [ 'init','-d', projectPath ], {}, onComplete )
 		}
