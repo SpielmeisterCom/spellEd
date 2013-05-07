2a3a5b0c93443e489103faa7dbf3c91bab7223d7
Ioannis Kostomanolakis <ik@spielmeister.com>
2013-05-07 16:23:33 +0200

    SPELLJS-1174 - show buildNumber and buildTimeStamp (from Configuration) in the about dialog - Resolved

--- public/app/view/ui/SpelledAboutDialog.js
+++ public/app/view/ui/SpelledAboutDialog.js
@@ -4,8 +4,6 @@ Ext.define('Spelled.view.ui.SpelledAboutDialog' ,{
 
     alias: 'widget.spelledabout',
 
-	layout: 'fit',
-
 	autoShow: true,
     modal : true,
     closable: true,
@@ -16,7 +14,26 @@ Ext.define('Spelled.view.ui.SpelledAboutDialog' ,{
 			this,{
 				items: [
 					{
-
+						xtype: 'image',
+						src: 'http://spelljs.com/wp-content/themes/spelljs/images/logo-spell-js.png'
+					},
+					{
+						xtype: 'textfield',
+						fieldLabel: 'Version',
+						value: Spelled.Configuration.version,
+						readOnly: true
+					},
+					{
+						xtype: 'textfield',
+						fieldLabel: 'Build number',
+						value: Spelled.Configuration.buildNumber,
+						readOnly: true
+					},
+					{
+						xtype: 'textfield',
+						fieldLabel: 'Build timestamp',
+						value: Spelled.Configuration.buildTimeStamp,
+						readOnly: true
 					}
 				]
 			}
