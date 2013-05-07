de6a7ef0274775e41ac09bbd6bf9099cf9c05d86
Julian Haupt <julian.haupt@spielmeister.com>
2013-05-07 19:18:16 +0200

    Build number => Build

--- public/app/view/ui/SpelledAboutDialog.js
+++ public/app/view/ui/SpelledAboutDialog.js
@@ -18,7 +18,7 @@ Ext.define('Spelled.view.ui.SpelledAboutDialog' ,{
                 html:
                     '<img src="resources/images/logo-spell-js.png"/><br/>' +
                     '<strong>SpellJS version ' + Spelled.Configuration.version + '</strong> ' +
-                    '&nbsp;&nbsp;&nbsp;Build number ' + Spelled.Configuration.buildNumber + '<br/>' +
+                    '&nbsp;&nbsp;&nbsp;Build ' + Spelled.Configuration.buildNumber + '<br/>' +
                     'built on ' + Spelled.Configuration.buildTimeStamp + '<br/><br/>' +
                     '&copy; 2011-2013 Spielmeister GmbH, Germany<br/><br/>' +
                     'Parts of this product use open source software.<br/>Please consult the licence details for more details.'
