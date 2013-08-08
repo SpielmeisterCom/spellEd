4dacfc1b84667474abf71f5af8e2809b2837a205
Martin Dederer <md@spielmeister.com>
2013-08-08 16:01:56 +0200

    Updated scene script template

--- public/app/controller/Scenes.js
+++ public/app/controller/Scenes.js
@@ -1094,7 +1094,21 @@ Ext.define('Spelled.controller.Scenes', {
 	createInitialSceneScriptContent: function( scene ) {
 		var parts = [
 			"return {",
+			"	/**",
+			"	 * This function is executed after the entites contained in the scene configuration have been created. All initialization functionality specific to",
+			"	 * the scene should be placed here.",
+			"	 *",
+			"	 * @param spell",
+			"	 * @param sceneConfig",
+			"	 */",
 			"	init : function( spell, sceneConfig ) {},",
+			"",
+			"	/**",
+			"	 * This function is executed before all entities are destroyed. Scene specific clean up should be performed here.",
+			"	 *",
+			"	 * @param spell",
+			"	 * @param sceneConfig",
+			"	 */",
 			"	destroy : function( spell, sceneConfig ) {}",
 			"}"
 		]
