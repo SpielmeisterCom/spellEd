c1f8280be29be15fe8c654e7751653b4e391b0ff
root <root@bigwhoop.(none)>
2013-08-26 11:23:10 +0200

    Changed version to 0.8.37

--- public/app/Configuration.js
+++ public/app/Configuration.js
@@ -65,7 +65,7 @@ Ext.define( 'Spelled.Configuration', {
 	updateServerUrl        : 'http://cdn.spelljs.com/spelljs-desktop-latest-version.json',
 	demoServerHostname     : 'spelled-demo.spelljs.com',
 	demoStagingServer      : 'spelled-demo.spelljs.staging',
-	version                : '0.8.36',
+	version                : '0.8.37',
 	buildNumber	           : '99999',
 	buildTimeStamp	       : '2099-01-01T01:00:00.000+01:00',
 	storageVersion         : 1,
