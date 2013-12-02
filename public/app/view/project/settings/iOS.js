62a950b76fa2ed6079f3af7641648be829519eef
Julian Haupt <julian.haupt@spielmeister.com>
2013-12-02 20:32:43 +0100

    added option for manually opening Xcode

--- public/app/view/project/settings/iOS.js
+++ public/app/view/project/settings/iOS.js
@@ -9,18 +9,42 @@ Ext.define('Spelled.view.project.settings.iOS' ,{
         Ext.applyIf( this, {
                 items: [
 	                {
-		                xtype: 'textfield',
-		                name: 'bundleId',
-		                fieldLabel: 'Bundle ID',
-		                anchor: '100%'
+		                xtype:'fieldset',
+		                title: 'App settings',
+		                defaults: {
+			                labelWidth: 130
+		                },
+		                items: [
+			                {
+				                xtype: 'textfield',
+				                name: 'bundleId',
+				                fieldLabel: 'Bundle ID',
+				                anchor: '100%'
+			                },
+			                {
+				                // Apple ID (a number) for your app from iTunes Connect.
+				                xtype: 'numberfield',
+				                name: 'appleId',
+				                fieldLabel: 'Apple ID',
+				                anchor: '100%'
+			                }
+		                ]
 	                },
 	                {
-		                // Apple ID (a number) for your app from iTunes Connect.
-		                xtype: 'numberfield',
-		                name: 'appleId',
-		                fieldLabel: 'Apple ID',
-		                anchor: '100%'
-	                }
+		                xtype:'fieldset',
+		                title: 'Build Options',
+		                defaults: {
+			                labelWidth: 130
+		                },
+		                items: [
+			                {
+				                boxLabel: 'Open Xcode project to manually debug/build the project',
+				                xtype: 'checkbox',
+				                inputValue: true,
+				                name: 'openXcode'
+			                }
+		                ]
+	                },
                 ]
             }
         )
