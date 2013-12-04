da9e883f30eb4655dc66aeb30c401bdbc740c756
Julian Haupt <julian.haupt@spielmeister.com>
2013-12-04 23:37:38 +0100

    added missing tizen resources

--- public/app/store/project/TizenResources.js
+++ public/app/store/project/TizenResources.js
@@ -14,6 +14,56 @@ Ext.define(
 					height:     117,
 					path:       'resources/tizen/icon.png'
 				}
+			},
+			{
+				name: 'author-ca-file',
+				description: 'cer file',
+				xtype: 'certificate',
+				xdata: {
+					path: 'resources/tizen/certs/tizen-developer-ca.cer'
+				}
+			},
+			{
+				name: 'author-key-file',
+				description: 'p12 file',
+				xtype: 'privatekey',
+				xdata: {
+
+				}
+			},
+			{
+				name: 'tizen-dist1-ca-file',
+				description: 'cer file',
+				xtype: 'certificate',
+				xdata: {
+					path: 'resources/tizen/certs/tizen-distributor-ca.cer'
+
+				}
+			},
+			{
+				name: 'tizen-dist1-key-file',
+				description: 'p12 file',
+				xtype: 'privatekey',
+				xdata: {
+
+				}
+			},
+			{
+				name: 'tizen-dist2-ca-file',
+				description: 'cer file',
+				xtype: 'certificate',
+				xdata: {
+					path: 'resources/tizen/certs/tizen-distributor-ca.cer'
+
+				}
+			},
+			{
+				name: 'tizen-dist2-key-file',
+				description: 'p12 file',
+				xtype: 'certificate',
+				xdata: {
+
+				}
 			}
 		]
 	}
