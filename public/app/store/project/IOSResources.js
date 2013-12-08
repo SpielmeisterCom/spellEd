fc13a64494a29f74e1a6e3b869d82344390697c1
Julian Haupt <julian.haupt@spielmeister.com>
2013-12-08 19:01:36 +0100

    added iOS image resources

--- public/app/store/project/IOSResources.js
+++ public/app/store/project/IOSResources.js
@@ -5,8 +5,146 @@ Ext.define(
 
 		fields: [ 'name', 'xtype', 'config' ],
 
-		data : [
-
-		]
+		data : [{
+			name: 'app-icon-57',
+			xtype: 'projectresourceimage',
+			config: {
+				title:				'Main Application Icon',
+				description:		'',
+				width:				57,
+				height:				57,
+				path:				'resources/ios/Images.xcassets/AppIcon.appiconset/icon57.png'
+			}
+		}, {
+			name: 'app-icon-72',
+			xtype: 'projectresourceimage',
+			config: {
+				title:				'Main Application Icon',
+				description:		'',
+				width:				72,
+				height:				72,
+				path:				'resources/ios/Images.xcassets/AppIcon.appiconset/icon72.png'
+			}
+		}, {
+			name: 'app-icon-76',
+			xtype: 'projectresourceimage',
+			config: {
+				title:				'Main Application Icon',
+				description:		'iOS 7',
+				width:				76,
+				height:				76,
+				path:				'resources/ios/Images.xcassets/AppIcon.appiconset/icon76.png'
+			}
+		}, {
+			name: 'app-icon-114',
+			xtype: 'projectresourceimage',
+			config: {
+				title:				'Main Application Icon',
+				description:		'',
+				width:				114,
+				height:				114,
+				path:				'resources/ios/Images.xcassets/AppIcon.appiconset/icon114.png'
+			}
+		}, {
+			name: 'app-icon-120',
+			xtype: 'projectresourceimage',
+			config: {
+				title:				'Main Application Icon',
+				description:		'iOS7',
+				width:				120,
+				height:				120,
+				path:				'resources/ios/Images.xcassets/AppIcon.appiconset/icon120.png'
+			}
+		}, {
+			name: 'app-icon-144',
+			xtype: 'projectresourceimage',
+			config: {
+				title:				'Main Application Icon',
+				description:		'',
+				width:				144,
+				height:				144,
+				path:				'resources/ios/Images.xcassets/AppIcon.appiconset/icon144.png'
+			}
+		}, {
+			name: 'app-icon-120',
+			xtype: 'projectresourceimage',
+			config: {
+				title:				'Main Application Icon',
+				description:		'iOS7',
+				width:				152,
+				height:				152,
+				path:				'resources/ios/Images.xcassets/AppIcon.appiconset/icon152.png'
+			}
+		}, {
+			name: 'launchimage',
+			xtype: 'projectresourceimage',
+			config: {
+				title:				'Main Application Launchimage',
+				description:		'',
+				width:				320,
+				height:				480,
+				path:				'resources/ios/Images.xcassets/LaunchImage.launchimage/Default.png'
+			}
+		}, {
+			name: 'launchimage-2x',
+			xtype: 'projectresourceimage',
+			config: {
+				title:				'Main Application Launchimage',
+				description:		'',
+				width:				640,
+				height:				960,
+				path:				'resources/ios/Images.xcassets/LaunchImage.launchimage/Default@2x.png'
+			}
+		}, {
+			name: 'launchimage-568h-2x',
+			xtype: 'projectresourceimage',
+			config: {
+				title:				'Main Application Launchimage',
+				description:		'',
+				width:				640,
+				height:				1136,
+				path:				'resources/ios/Images.xcassets/LaunchImage.launchimage/Default-568h@2x.png'
+			}
+		}, {
+			name: 'launchimage-landscape-ipad',
+			xtype: 'projectresourceimage',
+			config: {
+				title:				'Main Application Launchimage',
+				description:		'',
+				width:				1024,
+				height:				768,
+				path:				'resources/ios/Images.xcassets/LaunchImage.launchimage/Default-Landscape~ipad.png'
+			}
+		}, {
+			name: 'launchimage-landscape-ipad-2x',
+			xtype: 'projectresourceimage',
+			config: {
+				title:				'Main Application Launchimage',
+				description:		'',
+				width:				2048,
+				height:				1536,
+				path:				'resources/ios/Images.xcassets/LaunchImage.launchimage/Default-Landscape@2x~ipad.png'
+			}
+		}, {
+			name: 'launchimage-potrait-ipad',
+			xtype: 'projectresourceimage',
+			config: {
+				title:				'Main Application Launchimage',
+				description:		'',
+				width:				768,
+				height:				1024,
+				path:				'resources/ios/Images.xcassets/LaunchImage.launchimage/Default-Landscape~ipad.png'
+			}
+		}, {
+			name: 'launchimage-portrait-ipad-2x',
+			xtype: 'projectresourceimage',
+			config: {
+				title:				'Main Application Launchimage',
+				description:		'',
+				width:				1536,
+				height:				2048,
+				path:				'resources/ios/Images.xcassets/LaunchImage.launchimage/Default-Landscape@2x~ipad.png'
+			}
+		}]
 	}
 );
